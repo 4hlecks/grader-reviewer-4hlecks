cf6093a3cf9d46c125010a72dbcbe8a66326807d
4hlecks <alatienza@ucsd.edu>
2023-03-01 13:53:00 -0800

    part 1

--- grade.sh
+++ grade.sh
@@ -3,7 +3,7 @@ CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 rm ListExamples*
 rm *.class
 rm -rf student-submission
-rm errorFIle.txt
+rm errorFile.txt
 git clone $1 student-submission
 echo 'Finished cloning'
 
@@ -12,13 +12,14 @@ echo 'In submission directory'
 
 cp ListExamples.java ..
 cd ..
+GRADE=100
 
 if [[ -f ListExamples.java ]]
 then
     echo 'ListExamples found'
 else
     echo 'Missing File ListExamples.java'
-    exit 1
+    GRADE-=100
 fi
 
 FILTER=`grep -c -i "static List<String> filter(List<String> list, StringChecker sc)" ListExamples.java`
@@ -29,7 +30,7 @@ then
     echo "Filter method found."
 else
     echo "Filter method not found."
-    exit 1
+    GRADE-=50
 fi
 
 if [[ $MERGE -eq 1 ]]
@@ -37,14 +38,7 @@ then
     echo "Merge method found."
 else
     echo "Merge method not found."
-    exit 1
-fi
-
-if [[ $? -eq 0 ]]
-then
-    echo "Implementation passed."
-else
-    echo "Implementation failed"
+    GRADE-=50
 fi
 
 javac -cp $CPATH *.java
@@ -53,7 +47,10 @@ java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > errorFile.txt 2>&1
 if grep -q "OK" errorFile.txt
 then
     echo "Great job!"
-    echo "Grade: 100%"
+    echo "Grade: 100/100"
 else
-    echo `grep -e "failure" errorFile.txt`
+    FAILURES=`grep -c "Failure" errorFile.txt`
+    FAILURES*=25
+    GRADE-=FAILURES
+    echo "Grade: "$GRADE"/100"
 fi
